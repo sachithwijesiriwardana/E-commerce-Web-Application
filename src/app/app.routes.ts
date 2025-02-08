77409fdd7df60928ac305270164a3d00c732aabb
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-02-09 00:28:27 +0530

    add new components for my order, products, and create order with styles and templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { CreateOrderComponent } from './pages/create-order/create-order.component';
+import { MyorderComponent } from './pages/myorder/myorder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: ProductsComponent },
+  { path: 'create-order', component: CreateOrderComponent },
+  { path: 'my-order', component: MyorderComponent },
+];
