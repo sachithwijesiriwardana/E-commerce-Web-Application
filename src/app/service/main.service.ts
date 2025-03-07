57bc934d87456357cecc88a0310bd0566b654c47
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-03-07 23:13:56 +0530

    add navigation bar and product service with API integration

---
+++ src/app/service/main.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { APIResponseModel } from '../model/Product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MainService {
+  apiUrl: string = 'https://freeapi.miniprojectideas.com/api/BigBasket/';
+
+  constructor(private http: HttpClient) {}
+
+  getAllProducts(): Observable<APIResponseModel> {
+    return this.http.get<APIResponseModel>(this.apiUrl + 'GetAllProducts');
+  }
+}
